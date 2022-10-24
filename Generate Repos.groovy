d51b5d046f3d196f7bed689c7b7837cb54e0321d
shengwen zhu <172918388@qq.com>
2022-10-24 20:59:54 +0800

    Initial commit

---
+++ Generate Repos.groovy
@@ -0,0 +1,75 @@
+import com.intellij.database.model.DasTable
+import com.intellij.database.model.ObjectKind
+import com.intellij.database.util.Case
+import com.intellij.database.util.DasUtil
+import java.io.*
+import java.text.SimpleDateFormat
+
+/*
+ * Available context bindings:
+ *   SELECTION   Iterable<DasObject>
+ *   PROJECT     project
+ *   FILES       files helper
+ */
+packageName = ""
+
+
+
+FILES.chooseDirectoryAndSave("Choose directory", "Choose where to store generated files") { dir ->
+    SELECTION.filter { it instanceof DasTable && it.getKind() == ObjectKind.TABLE }.each { generate(it, dir) }
+}
+
+def generate(table, dir) {
+    def className = javaClassName(table.getName(), true)
+    packageName = getPackageName(dir)
+    PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(new File(dir, className + "Repository.java")), "UTF-8"))
+    printWriter.withPrintWriter { out -> generate(out, className, table) }
+
+//    new File(dir, className + ".java").withPrintWriter { out -> generate(out, className, fields,table) }
+}
+
+// 获取包所在文件夹路径
+def getPackageName(dir) {
+    return dir.toString().replaceAll("\\\\", ".").replaceAll("/", ".").replaceAll("^.*src(\\.main\\.java\\.)?", "") + ";"
+}
+
+def generate(out, className, table) {
+    out.println "package $packageName"
+    out.println ""
+    out.println "import org.springframework.stereotype.Repository;"
+    out.println "import org.springframework.data.jpa.repository.JpaRepository;"
+    out.println "import com.jeiat.itapi.model.$className;"
+
+    out.println ""
+    out.println "/**\n" +
+          " * Date " + new SimpleDateFormat("yyyy-MM-dd").format(new Date()) + " \n" +
+          " */"
+    out.println ""
+    out.println "@Repository"
+    out.println "public interface ${className}Repository extends JpaRepository<$className, Long> {"
+    out.println ""
+    out.println ""
+    out.println "}"
+}
+
+
+def javaClassName(str, capitalize) {
+    def s = com.intellij.psi.codeStyle.NameUtil.splitNameIntoWords(str)
+            .collect { Case.LOWER.apply(it).capitalize() }
+            .join("")
+            .replaceAll(/[^\p{javaJavaIdentifierPart}[_]]/, "_")
+    capitalize || s.length() == 1 ? s : Case.LOWER.apply(s[0]) + s[1..-1]
+}
+
+def javaName(str, capitalize) {
+    def s = com.intellij.psi.codeStyle.NameUtil.splitNameIntoWords(str)
+            .collect { Case.LOWER.apply(it).capitalize() }
+            .join("")
+            .replaceAll(/[^\p{javaJavaIdentifierPart}[_]]/, "_")
+    capitalize || s.length() == 1 ? s : Case.LOWER.apply(s[0]) + s[1..-1]
+}
+
+def isNotEmpty(content) {
+    return content != null && content.toString().trim().length() > 0
+}
+
