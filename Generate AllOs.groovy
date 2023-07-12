4916549dc8f3451c35645813f0885751b77202c6
zhushengwen <zsw@126.com>
2023-07-12 11:53:25 +0800

    解决乱码问题

--- Generate AllOs.groovy
+++ Generate AllOs.groovy
@@ -1,8 +1,9 @@
 import java.io.*
+import com.intellij.database.extensions.ExtensionScriptsUtil.*
 
 String dir = "C:\\Users\\Administrator\\AppData\\Roaming\\JetBrains\\IntelliJIdea2022.2\\extensions\\com.intellij.database\\schema\\"
 
-evaluate(new File(dir + "Generate POJOs.groovy"))
-evaluate(new File(dir + "Generate Repos.groovy"))
-evaluate(new File(dir + "Generate Srvcs.groovy"))
-evaluate(new File(dir + "Generate Views.groovy"))
+evaluate(new File(dir + "Generate POJOs.groovy").getText('UTF-8'))
+evaluate(new File(dir + "Generate Repos.groovy").getText('UTF-8'))
+evaluate(new File(dir + "Generate Srvcs.groovy").getText('UTF-8'))
+evaluate(new File(dir + "Generate Views.groovy").getText('UTF-8'))
